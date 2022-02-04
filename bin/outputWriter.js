419ddf637643741cd038865b175b79fef5d722be
Yuval Shalgi <yuvals@perfectomobile.com>
2022-02-04 17:45:28 +0200

    nicerNames

--- bin/outputWriter.js
+++ bin/outputWriter.js
@@ -2,15 +2,15 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = ( filename, jsonData) => {
-    logResultToconsole();
-    writeFileOutput();
+    consoleLog();
+    writeFile();
 
-    function writeFileOutput() {
+    function writeFile() {
         const newFileName = path.join(__dirname, '../output', filename);
         fs.writeFileSync(newFileName, jsonData);
     }
 
-    function logResultToconsole() {
+    function consoleLog() {
         console.log(`fileNam: \n ${filename}\n`);
         console.log(`jsonData:\n ${jsonData}`);
     }
