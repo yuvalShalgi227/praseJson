4476eabc0b1d4f2bfebc3e2b48f487fad8626d14
Yuval Shalgi <yuvals@perfectomobile.com>
2022-02-04 17:40:45 +0200

    extract functions

--- bin/outputWriter.js
+++ bin/outputWriter.js
@@ -2,8 +2,16 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = ( filename, jsonData) => {
-    console.log('fileNam: '+ filename);
-    console.log(`\njsonData:\n\n ${jsonData}`);
-    const newFileName = path.join(__dirname, '../output',filename)
-    fs.writeFileSync(newFileName,jsonData)
+    logResultToconsole();
+    writeFileOutput();
+
+    function writeFileOutput() {
+        const newFileName = path.join(__dirname, '../output', filename);
+        fs.writeFileSync(newFileName, jsonData);
+    }
+
+    function logResultToconsole() {
+        console.log(`fileNam: \n ${filename}\n`);
+        console.log(`jsonData:\n ${jsonData}`);
+    }
 }
