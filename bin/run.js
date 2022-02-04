250f30d7b9accf22f99cf85bc0d33e1d4f86757b
Yuval Shalgi <yuvals@perfectomobile.com>
2022-02-04 17:21:53 +0200

    even smaller functions

--- bin/run.js
+++ bin/run.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const output = require('./outputWriter')
-const getUpdatedJsonData = require('../lib/json-refactor/StepsCommands')
+const getUpdatedJsonData = require('../lib/json-refactor/refactorCommandStepsExpectedData')
 
 const resourceFolder= path.join(__dirname,'../resources')
 const files = fs.readdirSync(resourceFolder);
