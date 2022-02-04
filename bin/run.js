de3801176a20803496a6bd14b772508be84bc098
Yuval Shalgi <yuvals@perfectomobile.com>
2022-02-04 17:59:19 +0200

    refactor

--- bin/run.js
+++ bin/run.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const output = require('./outputWriter')
-const getUpdatedJsonData = require('../lib/json-refactor/commands')
+const getUpdatedJsonData = require('../lib/json-refactor/changeJson')
 
 const resourceDir= path.join(__dirname,'../resources')
 const filesInFolder = fs.readdirSync(resourceDir);
